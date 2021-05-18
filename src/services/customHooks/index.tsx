bd2c8edb991e9e8e670377001dbf56bbe8d542b1
MiguelNeto16 <miguel.neto320@gmail.com>
2021-05-17 23:35:24 -0300

    feat: create money custom hook

--- src/services/customHooks/index.tsx
+++ src/services/customHooks/index.tsx
@@ -1,13 +1,12 @@
 export const useMoney = () => {
     return function (money: string | number) {
-        return money.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+        return money.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
     }
 }
 
 export const Uuid = () => {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 || 0x8);
-      return v.toString(16);
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 || 0x8);
+        return v.toString(16);
     });
-  }
-  
+}
